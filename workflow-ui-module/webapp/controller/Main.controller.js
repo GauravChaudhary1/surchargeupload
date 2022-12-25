d8bc9eb9403968f616af420c33ab0d827c514303
Gaurav Chaudhary <gauravchaudhary.ait@gmail.com>
2022-12-25 11:25:04 +0000

    Add Submit Button

--- workflow-ui-module/webapp/controller/Main.controller.js
+++ workflow-ui-module/webapp/controller/Main.controller.js
@@ -9,10 +9,38 @@ sap.ui.define([
 
         return Controller.extend("com.zgaur.fiori.workflowuimodule.controller.Main", {
             onInit: function () {
-                
+                this.getOwnerComponent().getInboxAPI().addAction(
+                    {
+                      action: "Submit",
+                      label: "Submit"
+                    },
+                    function () {
+                      this.submitResponses();
+                    },
+                    this
+                  );
             },
             EditableAction: function(sAction){
                 return (sAction === "Approved" || sAction === "Rejected") ? false : true;
+            },
+
+            submitResponses: async function(){
+                const oResponse = await fetch(`${this.getOwnerComponent()._getTaskInstancesBaseURL()}`);
+
+                const oJSON = await oResponse.json();
+                const sWorkflowId = oJSON["workflowInstanceId"];
+
+                let oData = this.getView().byId("idResponseTable").getBinding("items").getModel().getData();
+
+                const oPatch = await fetch(`${this.getOwnerComponent()._getWorkflowRuntimeBaseURL()}/workflow-instances/${sWorkflowId}/context`,{
+                    method: "PATCH",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        "X-CSRF-Token": this.getOwnerComponent()._fetchToken()
+                    },
+                    body: JSON.stringify(oData)
+                });
+                const oPatchJSON = await oPatch.json();
             }
         });
     });
