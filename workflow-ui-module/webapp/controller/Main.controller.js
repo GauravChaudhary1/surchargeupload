a30dada796dca989470b2b39d128dcc6f92ce4a4
Gaurav Chaudhary <gauravchaudhary.ait@gmail.com>
2023-01-15 05:40:51 +0000

    Multi Response

--- workflow-ui-module/webapp/controller/Main.controller.js
+++ workflow-ui-module/webapp/controller/Main.controller.js
@@ -23,6 +23,34 @@ sap.ui.define([
             EditableAction: function(sAction){
                 return (sAction === "Approved" || sAction === "Rejected") ? false : true;
             },
+            selectResponse: function(oEvent){
+                const oSelectedIndex = oEvent.getParameter('selectedIndex');
+                const sPurchaseOrder = oEvent.getSource().getParent().getCells()[1].getText();
+                const sPurchaseOrderItem = oEvent.getSource().getParent().getCells()[2].getText();
+
+                this.getView().byId("idResponseTable").getBinding("rows").aIndices.forEach(i => {
+                    if (this.getView().byId("idResponseTable").getAggregation('rows')[i].getCells()[1].getText() === sPurchaseOrder && 
+                        this.getView().byId("idResponseTable").getAggregation('rows')[i].getCells()[2].getText() === sPurchaseOrderItem) {                        
+                            this.getView().byId("idResponseTable").getAggregation('rows')[i].getCells()[0].setSelectedIndex(oSelectedIndex);                    
+                    }                                        
+                })
+            },
+            setAction: function(sAction){
+                let i = 3;
+                switch (sAction) {
+                    case "Approved":
+                        i = 0;
+                        break;
+                    case "Rejected":
+                        i = 1;
+                        break;
+                
+                    default:
+                        i = 2;
+                        break;
+                }
+                return i;
+            },
 
             submitResponses: async function(){
                 const oResponse = await fetch(`${this.getOwnerComponent()._getTaskInstancesBaseURL()}`);
@@ -30,7 +58,26 @@ sap.ui.define([
                 const oJSON = await oResponse.json();
                 const sWorkflowId = oJSON["workflowInstanceId"];
 
-                let oData = this.getView().byId("idResponseTable").getBinding("items").getModel().getData();
+                let oData = this.getView().byId("idResponseTable").getBinding("rows").getModel().getData();
+
+                this.getView().byId("idResponseTable").getBinding("rows").aIndices.forEach(i => {
+                    
+                    
+                    switch (this.getView().byId("idResponseTable").getAggregation('rows')[i].getCells()[0].getSelectedIndex()) {
+                        case 0:
+                            oData.filecontent[i]["ActionPerformed"] = "Approved";
+                            break;
+                        case 1:
+                            oData.filecontent[i]["ActionPerformed"] = "Rejected";
+                            break;
+                        default:
+                            break;
+                    }
+                });
+                this.getView().byId("idResponseTable").getBinding("rows").getModel().setData(oData);
+                this.getView().byId("idResponseTable").getBinding("rows").getModel().refresh(true);
+
+                // Make backend Calls
 
                 const oPatch = await fetch(`${this.getOwnerComponent()._getWorkflowRuntimeBaseURL()}/workflow-instances/${sWorkflowId}/context`,{
                     method: "PATCH",
@@ -40,7 +87,15 @@ sap.ui.define([
                     },
                     body: JSON.stringify(oData)
                 });
-                const oPatchJSON = await oPatch.json();
+
+
+                // if all action is taken then close the task
+                if ( !oData.filecontent.find((el) =>{ return el["ActionPerformed"] === "" } ) ){
+                    this.getOwnerComponent().completeTask();
+                };
+
+                // Refresh the task list
+                this.getOwnerComponent()._refreshTaskList();
             }
         });
     });
